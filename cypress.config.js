15179472bfc45a97dd4ece5b09f193d9fbddfb18
EtienneBrunet <51758248+EtienneBrunet@users.noreply.github.com>
2024-11-14 15:51:18 +0100

    Update cypress.config.js
    delete pg config

--- cypress.config.js
+++ cypress.config.js
@@ -1,27 +1,12 @@
 const cucumber = require('cypress-cucumber-preprocessor').default;
 const { defineConfig } = require("cypress");
-const pg = require("pg");
 
 module.exports = defineConfig({
   
   e2e: { 
     setupNodeEvents(on, config) {
-       on('file:preprocessor', cucumber()),
-       on('task', {
-        READFROMDB({dbConfig,sql})
-        {
-          const client = new pg.Pool(dbConfig);
-          return client.query(sql);
-        }
-       })
+       on('file:preprocessor', cucumber())
     }, 
     specPattern: "cypress/e2e/*.feature", 
-  },
-  UAT : {
-    user: 'postgres',
-    host: 'localhost',
-    database: 'postgres',
-    password: '1344',
-    port: '5432'
   }
 });
